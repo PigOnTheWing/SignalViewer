feab4005ee4393a1950c8590e6ee30fa530c3896
Smol <nekit.smol@gmail.com>
2020-11-18 18:31:27 +0300

    Main refactoring

--- SignalServer/main.cpp
+++ SignalServer/main.cpp
@@ -9,10 +9,15 @@
 #include "settingswrapper.h"
 
 Logger g_logger;
-quint16 port;
 
-void appInit(QStringList &);
+int g_argc;
+char **g_argv;
+
+void appInit();
 void close();
+
+void parseArgs(QCommandLineParser &, QString *);
+bool parseConfig(QString &, quint16 *);
 
 #ifdef Q_OS_WIN
 #include <windows.h>
@@ -22,9 +27,106 @@ void close();
 SERVICE_STATUS g_svcStatus;
 SERVICE_STATUS_HANDLE g_svcStatusHandle;
 
-VOID WINAPI ctrlHandler(DWORD dwCtrl);
+VOID WINAPI ctrlHandler(DWORD);
 VOID reportStatus(DWORD, DWORD, DWORD);
 
+VOID WINAPI ServiceMain(int, char **);
+#endif
+
+int main(int argc, char *argv[])
+{
+    g_argc = argc;
+    g_argv = argv;
+
+#ifdef Q_OS_WIN
+    SERVICE_TABLE_ENTRY DispatchTable[] = {
+        { SVCNAME, (LPSERVICE_MAIN_FUNCTION) ServiceMain },
+        { NULL, NULL }
+    };
+
+    if (!StartServiceCtrlDispatcher(DispatchTable)) {
+        if (GetLastError() != ERROR_FAILED_SERVICE_CONTROLLER_CONNECT) {
+            return 0;
+        }
+    }
+#endif
+
+    appInit();
+    close();
+}
+
+void messageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
+    g_logger.handleMessage(type, context, msg);
+}
+
+void appInit() {
+    QCoreApplication a(g_argc, g_argv);
+
+    QString configFile;
+    QCommandLineParser parser;
+    parseArgs(parser, &configFile);
+
+    if (configFile == "") {
+        qCritical("No config option given, exiting");
+        close();
+    }
+
+    quint16 port;
+    if (!parseConfig(configFile, &port)) {
+        close();
+    }
+
+    qInstallMessageHandler(messageHandler);
+
+    SignalServer signalServer;
+    if (!signalServer.startListening(port)) {
+        qCritical("Could not start the server");
+        close();
+    }
+
+#ifdef Q_OS_WIN
+    reportStatus(SERVICE_RUNNING, NO_ERROR, 0);
+#endif
+
+    a.exec();
+}
+
+void parseArgs(QCommandLineParser &parser, QString *configFile) {
+    parser.setApplicationDescription("A server that generates continuous signal with a given period and amplitude");
+    parser.addHelpOption();
+
+    QCommandLineOption configOption(QStringList() << "c" << "config" << "path", "A path to the configuration file", "path to config file");
+    parser.addOption(configOption);
+
+    parser.process(QCoreApplication::arguments());
+
+    *configFile = parser.value(configOption);
+}
+
+bool parseConfig(QString &configFile, quint16 *port) {
+    SettingsWrapper settings(configFile);
+
+    *port = settings.getPort();
+
+    g_logger.setLevel(settings.getLoggingLevel());
+    g_logger.setOutputOptions(settings.getOutputOptions());
+    if (!g_logger.setLoggingFile(settings.getLoggingFile())) {
+        qCritical("Could not open logging file");
+        return false;
+    }
+    g_logger.setLoggingFilters();
+
+    return true;
+}
+
+void close() {
+#ifdef Q_OS_WIN
+    reportStatus(SERVICE_STOPPED, NO_ERROR, 0);
+#endif
+    exit(EXIT_SUCCESS);
+}
+
+#ifdef Q_OS_WIN
 VOID WINAPI ServiceMain(int argc, char *argv[])
 {
     g_svcStatusHandle = RegisterServiceCtrlHandler(SVCNAME, ctrlHandler);
@@ -38,21 +140,7 @@ VOID WINAPI ServiceMain(int argc, char *argv[])
 
     reportStatus(SERVICE_START_PENDING, NO_ERROR, 3000);
 
-    QCoreApplication a(argc, argv);
-
-    g_logger.setOutputOptions(Logger::File);
-
-    qInfo("Args count: %d, last arg - %s", argc, argv[argc - 1]);
-
-    reportStatus(SERVICE_RUNNING, NO_ERROR, 0);
-
-    SignalServer signalServer;
-    if (!signalServer.startListening(port)) {
-        qCritical("Could not start the server");
-        close();
-    }
-
-    a.exec();
+    appInit();
     close();
 }
 
@@ -91,108 +179,3 @@ VOID reportStatus(DWORD currentState, DWORD exitCode, DWORD waitHint) {
 }
 
 #endif
-
-void messageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
-    g_logger.handleMessage(type, context, msg);
-}
-
-int main(int argc, char *argv[])
-{
-    QStringList args;
-    for (int i = 0; i < argc; ++i) {
-        args << argv[i];
-    }
-    appInit(args);
-
-#ifdef Q_OS_WIN
-    SERVICE_TABLE_ENTRY DispatchTable[] = {
-        { SVCNAME, (LPSERVICE_MAIN_FUNCTION) ServiceMain },
-        { NULL, NULL }
-    };
-
-    if (!StartServiceCtrlDispatcher(DispatchTable)) {
-        if (GetLastError() != ERROR_FAILED_SERVICE_CONTROLLER_CONNECT) {
-            return 0;
-        }
-    }
-#endif
-
-    QCoreApplication a(argc, argv);
-    g_logger.setLoggingFilters();
-
-    SignalServer signalServer;
-    if (!signalServer.startListening(port)) {
-        qCritical("Could not start the server");
-        close();
-    }
-
-    a.exec();
-    close();
-}
-
-bool parseArgs(QCommandLineParser &parser, QStringList &args, QString *configFile) {
-    parser.setApplicationDescription("A server that generates continuous signal with a given period and amplitude");
-    //parser.addHelpOption();
-
-    QCommandLineOption configOption(QStringList() << "c" << "config" << "path", "A path to the configuration file", "path to config file");
-    parser.addOption(configOption);
-
-    QCommandLineOption helpOption(QStringList() << "h" << "help", "Show help");
-    parser.addOption(helpOption);
-
-    if (!parser.parse(args)) {
-        qInfo("Server usage:\n-h, --help - Show help;\n"
-              "-c <config>, --config=<config> - Use config file, <config> - path to ini file with configuration");
-        return false;
-    }
-
-    if (parser.isSet(helpOption)) {
-        qInfo("Server usage:\n-h, --help - Show help;\n"
-              "-c <config>, --config=<config> - Use config file, <config> - path to ini file with configuration");
-        return false;
-    }
-
-    *configFile = parser.value(configOption);
-    return true;
-}
-
-bool parseConfig(QString &configFile) {
-    SettingsWrapper settings(configFile);
-
-    port = settings.getPort();
-
-    g_logger.setLevel(settings.getLoggingLevel());
-    g_logger.setOutputOptions(settings.getOutputOptions());
-    if (!g_logger.setLoggingFile(settings.getLoggingFile())) {
-        qCritical("Could not open logging file");
-        return false;
-    }
-
-    return true;
-}
-
-void close() {
-#ifdef Q_OS_WIN
-    reportStatus(SERVICE_STOPPED, NO_ERROR, 0);
-#endif
-    exit(EXIT_SUCCESS);
-}
-
-void appInit(QStringList &args) {
-    QString configFile;
-    QCommandLineParser parser;
-    if (!parseArgs(parser, args, &configFile)) {
-        close();
-    }
-
-    if (configFile == "") {
-        qCritical("No config option given, exiting");
-        close();
-    }
-
-    if (!parseConfig(configFile)) {
-        close();
-    }
-
-    qInstallMessageHandler(messageHandler);
-}
